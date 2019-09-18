5d8545784cc4268bba175d11e8987604a9107f97
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2019-09-18 22:38:34 +0200

    chore: prettier formatting

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,65 +1,65 @@
-import { $, create, detectScrollBar } from "./utils.js";
-import Vivus from "./vivus.js";
-import { showLines } from "./lines.js";
+import { $, create, detectScrollBar } from './utils.js'
+import Vivus from './vivus.js'
+import { showLines } from './lines.js'
 
 /* Smoothscroll-Polyfills laden */
-if (!("scrollBehavior" in create("a").style)) {
-  const smoothscrollPolyfill = create("script", {
-    src: "https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js",
-    defer: true
-  });
-  const smoothscrollAnchorPolyfill = create("script", {
-    src: "https://unpkg.com/smoothscroll-anchor-polyfill",
-    defer: true
-  });
+if (!('scrollBehavior' in create('a').style)) {
+  const smoothscrollPolyfill = create('script', {
+    src: 'https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js',
+    defer: true,
+  })
+  const smoothscrollAnchorPolyfill = create('script', {
+    src: 'https://unpkg.com/smoothscroll-anchor-polyfill',
+    defer: true,
+  })
 
-  document.head.appendChild(smoothscrollPolyfill);
-  document.head.appendChild(smoothscrollAnchorPolyfill);
+  document.head.appendChild(smoothscrollPolyfill)
+  document.head.appendChild(smoothscrollAnchorPolyfill)
 }
 
-const splashHeaderContainer = $(".splash-header-container");
+const splashHeaderContainer = $('.splash-header-container')
 
 function animateSplashLogo() {
-  document.body.classList.add("overflow-hidden");
-  const splash = $(".splash", splashHeaderContainer);
-  const splashLogo = $("svg", splash);
+  document.body.classList.add('overflow-hidden')
+  const splash = $('.splash', splashHeaderContainer)
+  const splashLogo = $('svg', splash)
 
   new Vivus(
     splashLogo,
     {
-      type: "oneByOne",
+      type: 'oneByOne',
       duration: 110,
       pathTimingFunction: Vivus.EASE_OUT_BOUNCE,
       onReady: () => {
-        setTimeout(() => splashLogo.classList.add("show-dot"), 1250);
-      }
+        setTimeout(() => splashLogo.classList.add('show-dot'), 1250)
+      },
     },
     () => {
-      splash.classList.add("splash--visible");
+      splash.classList.add('splash--visible')
       setTimeout(() => {
-        document.body.classList.remove("overflow-hidden");
-        splashHeaderContainer.classList.remove("h-full");
-        showLines();
-      }, 700);
+        document.body.classList.remove('overflow-hidden')
+        splashHeaderContainer.classList.remove('h-full')
+        showLines()
+      }, 700)
     }
-  );
+  )
 }
 
-if (splashHeaderContainer.classList.contains("h-full")) {
-  animateSplashLogo();
-} else showLines();
+if (splashHeaderContainer.classList.contains('h-full')) {
+  animateSplashLogo()
+} else showLines()
 
-if (typeof IntersectionObserver !== "undefined") {
-  const observedBtn = $(".js-observed-signup");
-  const headerBtn = $(".btn-signup");
+if (typeof IntersectionObserver !== 'undefined') {
+  const observedBtn = $('.js-observed-signup')
+  const headerBtn = $('.btn-signup')
 
   const observer = new IntersectionObserver(
     ([btn]) => {
-      if (btn.isIntersecting) headerBtn.classList.remove("lg:max-w-full");
-      else headerBtn.classList.add("lg:max-w-full");
+      if (btn.isIntersecting) headerBtn.classList.remove('lg:max-w-full')
+      else headerBtn.classList.add('lg:max-w-full')
     },
-    { rootMargin: "-64px 0px 0px" }
-  );
+    { rootMargin: '-64px 0px 0px' }
+  )
 
-  observer.observe(observedBtn);
+  observer.observe(observedBtn)
 }
--- docs/assets/js/utils.js
+++ docs/assets/js/utils.js
@@ -1,5 +1,5 @@
 /** @type {Document['querySelector']} */
-export const $ = (selector, root = document) => root.querySelector(selector);
+export const $ = (selector, root = document) => root.querySelector(selector)
 
 /**
  * @template {keyof HTMLElementTagNameMap} TagName
@@ -8,21 +8,21 @@ export const $ = (selector, root = document) => root.querySelector(selector);
  * @returns {HTMLElementTagNameMap[TagName]}
  */
 export const create = (tag, attrs = {}) => {
-  return Object.assign(document.createElement(tag), attrs);
-};
-
-export function detectScrollBar() {
-  const body = document.body;
-  const el = body.appendChild(create("div"));
-
-  el.style.cssText = `
-  width:100px;height:100px;overflow:scroll !important;position:absolute;top:-100vh`;
-
-  const hasScrollbar = el.offsetWidth - el.clientWidth > 0;
-  if (hasScrollbar) document.documentElement.classList.add("scrollbar");
-
-  body.removeChild(el);
-  return hasScrollbar;
+  return Object.assign(document.createElement(tag), attrs)
 }
 
-export const wait = time => new Promise(resolve => setTimeout(resolve, time));
+export function detectScrollBar() {
+  const body = document.body
+  const el = body.appendChild(create('div'))
+
+  el.style.cssText = `
+  width:100px;height:100px;overflow:scroll !important;position:absolute;top:-100vh`
+
+  const hasScrollbar = el.offsetWidth - el.clientWidth > 0
+  if (hasScrollbar) document.documentElement.classList.add('scrollbar')
+
+  body.removeChild(el)
+  return hasScrollbar
+}
+
+export const wait = time => new Promise(resolve => setTimeout(resolve, time))
